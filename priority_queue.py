c8f9f7d836723bf6907cbdc6e56029b6021ef78f
guilhermevbeira <guilherme.vieira.beira@gmail.com>
2019-08-24 14:11:44 -0300

    Implement priority queue

---
+++ priority_queue.py
@@ -0,0 +1,62 @@
+""" 
+Implementação da priority queue com list ordenada
+"""
+
+class Person:
+    def __init__(self, name, priority):
+        self.name = name
+        self.priority = priority
+    
+
+class PriorityQueue:
+    def __init__(self):
+        self.pq = []
+        self.length = 0
+    
+    def push(self, person):
+        if self.empty():
+            self.pq.append(person)
+        else:
+            flag_push = False
+            # procura-se onde inserir para manter a fila ordenada
+            for i in range(len(self.pq)):
+                if self.pq[i].priority < person.priority:
+                    self.pq.insert(i, person)
+                    flag_push = True
+                    break
+            if not flag_push:
+                #se entrou aqui é pq tem que inserir no final
+                self.pq.insert(len(self.pq), person)
+    
+    def pop(self):
+        if not self.empty():
+            self.pq.pop(0)
+
+    def empty(self):
+        return len(self.pq) == 0
+    
+    def show(self):
+        for p in self.pq:
+            print(f"Nome: {p.name}")
+            print(f"Prioridade {p.priority}")
+
+p1 = Person("Guilherme", 28)
+p2 = Person("Catariana", 3)
+p3 = Person("Pedro", 20)
+p4 = Person("Joao", 35)
+
+pq = PriorityQueue()
+pq.push(p1)
+pq.push(p2)
+pq.push(p3)
+pq.push(p4)
+# print("Exibindo as inserções")
+# pq.show()
+
+
+# print("Exibindo após as remoções")
+pq.pop()
+# pq.show()
+pq.push(Person("Goku", 30))
+print("Exibindo apos a inserção")
+pq.show()
